4327885de58b8be85d17efd9666b3e2fb0059244
Nikita <tolstik-nik@mail.ru>
2019-12-05 02:55:20 +0300

    Вынес функцию по отрисовке элементов в util.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 const TIME_FORMAT_NUMBER = 10;
 
 const castTimeFormat = (value) => {
@@ -15,3 +20,23 @@ export const formatTime = (date) => {
 
   return `${hours}:${minutes} ${interval}`;
 };
+
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
