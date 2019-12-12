ee79ebebf3498534a9f762c63ebff5cad1e43ca5
Nikita <tolstik-nik@mail.ru>
2019-12-12 17:52:54 +0300

    Создал класс TaskController,перенёс в него логику отрисовки карточки

---
+++ src/controllers/task-controller.js
@@ -0,0 +1,53 @@
+import TaskEditComponent from '../components/task-edit.js';
+import TaskComponent from '../components/task.js';
+import {render, RenderPosition, replace} from '../utils/render.js';
+
+
+export default class TaskController {
+  constructor(container, onDataChange) {
+
+    this._container = container;
+    this._onDataChange = onDataChange;
+  }
+
+  render(task) {
+
+
+    const taskComponent = new TaskComponent(task);
+    const taskEditComponent = new TaskEditComponent(task);
+
+    const onEscKeyDown = (evt) => {
+
+      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+      if (isEscKey) {
+        replaceEditToTask();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    const replaceEditToTask = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const replaceTaskToEdit = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    taskComponent.setEditButtonClickHandler(() => {
+      replaceTaskToEdit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+
+    taskEditComponent.setSubmitHandler(() => {
+      replaceEditToTask();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+
+    render(this._container, taskComponent, RenderPosition.BEFOREEND);
+
+  }
+
+}
